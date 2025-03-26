23304fcfb51d38ca50b631455d925fab7b85c80a
SamuelMancera <samumancardenas@gmail.com>
2025-03-26 14:26:15 -0500

    Update calculadoraFactorial.java

--- Turridez/calculadoraFactorial.java
+++ Turridez/calculadoraFactorial.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class calculadoraFactorial {
 public static void main(String[] args) {
         int numA = 0, numB = 0;
+        //Agregamos variable del número iniciada en 1, ya que con 0, la multiplicación siempre da 0 y con "long" para posibles números grandes//
         long numFactorial = 1;
         int varControl = 0;
+        //Variable del numero por el que se multiplica//
         int i = 1;
+        //Variable para el resultado factorial//
         long factorial = 1;
 
         Scanner scanner = new Scanner(System.in);
@@ -61,12 +64,18 @@ public static void main(String[] args) {
 
                     break;
                 case 5: 
+                 
+                    //Solicitamos el número y lo leemos//
                     System.out.print("Ingrese el número al que desea conocer el factorial:  ");
                     numFactorial = scanner.nextLong();
-
-                    while ( i <= numFactorial ) { factorial *= i; i++;}
-                    
-                    System.out.println( factorial );
+                    /*Aplicamos el ciclo confirmando primero la condición de que el número ingresado sea mayor o igual a 1, 
+                    ya que con cero siempre daría 0*/
+                    while ( i <= numFactorial )
+                    /*Guardamos las multiplicaciones en factorial, inciando desde i (1) hasta que i sea mayor o igual que el numero ingresado finalizando
+                    el proceso ya que con cada multiplicación i aumenta una cantidad*/
+                    { factorial *= i; i++;}
+                    //Imprimimos el resultado//
+                    System.out.println( numFactorial+"! = " + factorial);
 
                     
                     break;
